45eaec3fddb661705e7d742b5c05d43489618964
IvethS <ivethsabillon@hotmail.com>
2014-06-16 08:44:04 -0600

    Update MiClase.java

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,24 +1,29 @@
 public class MiClase
 {
 	//devuelve la suma de a y b
+	//en esta funcion solo sume las variables declaradas
 	static int sumarNumeros(int a, int b)
 	{
 		return a+b;
 	}
 	
 	//devuelve la resta de a y b
+	//en esta funcion solo reste las variables declaradas
 	static int restarNumeros(int a, int b)
 	{
 		return a-b;
 	}
 	
 	//devuelve la multiplicación de a y b
+	//en esta funcion hice la multiplicacion de variables
 	static int multiplicarNumeros(int a, int b)
 	{
 		return a*b;
 	}
 	
 	//devuelve true si num es par, de lo contrario, devuelve false
+	//aqui lo que hace es un calculo que todos los numeros que se dividen en dos
+	//devolvera verdadero y si no devolvera falso
 	static boolean esPar(int num)
 	{
 		 if(num%2==0)
@@ -30,6 +35,8 @@ public class MiClase
 	}
 	
 	//devuelve el numero mayor entre a, b o c
+	//se obtienen los valores de las variables declaradas, luego hace una comparacion
+	//y devuelve el menor de esos numeros
 	static int getMayor(int a, int b, int c)
 	{
 		  if(a > b)
@@ -44,6 +51,9 @@ public class MiClase
 	}
 	
 	//devuelve num elevado a la potencia
+	//aqui con la ayuda de la opcion matematica math pow 
+   	//podemos hacer el calculo directamente de la potencia
+   	//como pueden observar obtiene un resultado y luego devuelve la variable
 	static int potencia(int num, int potencia)
 	{
 		int resultado = (int) Math.pow(num, potencia);
@@ -51,6 +61,7 @@ public class MiClase
 	}
 	
 	//devuelve el sumas del arreglo
+	//estas lo obtuve con ayuda de: http://www.forosdelweb.com/f13/suma-valores-arreglo-591009/
 	static int sumarArreglo(int arreglo[])
 	{
 		
